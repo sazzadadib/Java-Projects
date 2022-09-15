bbb4ef8d97bfb166b235faa59ae35ee7702a607b
sazzadadib <sazzadhossainadib@gmail.com>
2022-09-16 00:38:44 +0600

    Calculator

---
+++ Calculator/src/calculator/Calculations.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author Sazzad
+ */
+public interface Calculations {
+    
+    void add(double a,double b);
+    void sub(double a,double b);
+    void mul(double a,double b);
+    void div(double a,double b);
+    
+}
